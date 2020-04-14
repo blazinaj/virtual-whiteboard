920a1e66d2ce52fbea3423db44c9d16433a0fa5b
Jacob Blazina <blazinaj@hotmail.com>
2020-04-14 16:09:39 -0700

    Converted Canvas.js to hooks

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 import CanvasDraw from "./RCD";
 import uuid from 'uuid/v4'
@@ -16,120 +16,134 @@ const colors = [
   '#FFD600',
   '#FF6D00',
   '#000000'
-]
+];
 
 function rand() {
   return colors[~~(colors.length * Math.random())];
 }
 
-class Demo extends Component {
-  state = {
-    brushColor: rand(),
-    canvasHeight: 350,
-    canvasWidth: 380,
-    brushRadius: 4,
-    lazyRadius: 8
-  }
-  lineLength = 0
-  id = '123'
-  clientId = uuid()
-  canvasInfo = 'tempcanvas'
-  componentDidMount() {
+const Canvas = (props) => {
+
+  const [brushColor, setBrushColor] = useState(rand());
+  const [canvasHeight, setCanvasHeight] = useState(500);
+  const [canvasWidth, setCanvasWidth] = useState(500);
+  const [brushRadius, setBrushRadius] = useState(4);
+  const [lazyRadius, setLazyRadius] = useState(8);
+
+  const canvasRef = useRef(null);
+  // const [canvasRef, setCanvasRef] = useState("");
+
+  let lineLength = 0;
+  const id = '123';
+  const clientId = uuid();
+  const canvasInfo = 'tempcanvas';
+
+  useEffect(() => {
     const canvas = {
-      id: this.id,
-      clientId: this.clientId,
+      id: id,
+      clientId: clientId,
       data: {
-        ...this.state,
+        canvasHeight,
+        canvasWidth,
+        brushRadius,
+        lazyRadius,
         lines: []
       }
-    }
+    };
+
     // Create the canvas. If canvas is already created, retrieve the data & draw previous canvas
     API.graphql(graphqlOperation(createCanvas, { input: canvas }))
-      .then(d => console.log('canvas created :', d))
-      .catch(err => {
-        if (err.errors[0].data.id === this.id) {
-          const d = err.errors[0].data.data
-          this.canvas.loadSaveData(d)
-        }
-      })
+        .then(d => console.log('canvas created :', d))
+        .catch(err => {
+          if (err.errors[0].data.id === id) {
+            const d = err.errors[0].data.data;
+            canvasRef.current.loadSaveData(d)
+          }
+        });
+
     window.addEventListener('mouseup', (e) => {
       // If we are clicking on a button, do not update anything
-      if (e.target.name === 'clearbutton') return 
-      this.setState({
-        brushColor: rand()
-      })
-      const data = this.canvas.getSaveData()
-      const p = JSON.parse(data)
-      const length = p.lines.length
-      this.lineLength = length
+      if (e.target.name === 'clearbutton') return;
+      setBrushColor(rand());
+
+      const data = canvasRef.current.getSaveData();
+      const p = JSON.parse(data);
+      const length = p.lines.length;
+      lineLength = length;
 
       const canvas = {
-        id: this.id,
-        clientId: this.clientId,
+        id: id,
+        clientId: clientId,
         data
-      }
+      };
+
       // Save updated canvas in the database
       API.graphql(graphqlOperation(updateCanvas, { input: canvas }))
-        .then(c => {
-          console.log('canvas updated!')
-        })
-        .catch(err => console.log('error creating: ', err))
-    })
+          .then(c => {
+            console.log('canvas updated!')
+          })
+          .catch(err => console.log('error creating: ', err))
+    });
+
     API.graphql(graphqlOperation(onUpdateCanvas))
-      .subscribe({
-        next: (d) => {
-          const data = JSON.parse(d.value.data.onUpdateCanvas.data)
-          const length = data.lines.length
-          if (length === 0) {
-            // If there is no canvas data, clear the canvas
-            const data = this.canvas.getSaveData()
-            const parsedData = JSON.parse(data)
-            const newData = {
-              ...parsedData,
-              lines: []
+        .subscribe({
+          next: (d) => {
+            const data = JSON.parse(d.value.data.onUpdateCanvas.data);
+            const length = data.lines.length;
+            if (length === 0) {
+              // If there is no canvas data, clear the canvas
+              const data = canvasRef.current.getSaveData();
+              const parsedData = JSON.parse(data);
+              const newData = {
+                ...parsedData,
+                lines: []
+              };
+              const newCanvas = JSON.stringify(newData);
+              canvasRef.current.loadSaveData(newCanvas);
+              return
             }
-            const newCanvas = JSON.stringify(newData)
-            this.canvas.loadSaveData(newCanvas)
-            return
+            if (lineLength === length || length === Number(0)) return;
+            // Draw new lines to canvas
+            const last = data.lines[length -1];
+            canvasRef.current.simulateDrawingLines({ lines: [last] })
           }
-          if (this.lineLength === length || length === Number(0)) return
-          // Draw new lines to canvas
-          const last = data.lines[length -1]
-          this.canvas.simulateDrawingLines({ lines: [last] })
-        }
-      })
-  }
-  clear = () => {
-    const data = this.canvas.getSaveData()
-    const parsedData = JSON.parse(data)
+        })
+  }, []);
+
+  const clear = () => {
+    const data = canvasRef.current.getSaveData();
+    const parsedData = JSON.parse(data);
     const newData = {
       ...parsedData,
       lines: []
-    }
-    const newCanvas = JSON.stringify(newData)
-    this.canvas.loadSaveData(newCanvas)
+    };
+    const newCanvas = JSON.stringify(newData);
+    canvasRef.current.loadSaveData(newCanvas);
     const canvas = {
-      id: this.id,
-      clientId: this.clientId,
+      id: id,
+      clientId: clientId,
       data: newCanvas
-    }
+    };
     API.graphql(graphqlOperation(updateCanvas, { input: canvas }))
         .then(c => {
           console.log('canvas cleared!')
         })
         .catch(err => console.log('error creating: ', err))
-  }
-  render() {
-    return (
-      <div>
-        <button name='clearbutton' onClick={this.clear}>Clear</button>
-        <CanvasDraw
-          {...this.state}
-          ref={canvas => this.canvas = canvas}
-        />
-      </div>
-    );
-  }
-}
+  };
 
-export default Demo
+  return (
+    <div>
+      <button name='clearbutton' onClick={() => clear()}>Clear</button>
+      <CanvasDraw
+        canvasWidth={canvasWidth}
+        canvasHeight={canvasHeight}
+        brushRadius={brushRadius}
+        lazyRadius={lazyRadius}
+        brushColor={brushColor}
+        ref={canvas => canvasRef.current = canvas}
+      />
+    </div>
+  );
+};
+
+export default Canvas

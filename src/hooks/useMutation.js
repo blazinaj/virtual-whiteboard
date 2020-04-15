352423c7d30be375b3de55e38aa19ed9344a1750
Jacob Blazina <blazinaj@hotmail.com>
2020-04-14 22:04:47 -0700

    Added cancel button, brush size input, and changed header color

--- src/hooks/useMutation.js
+++ src/hooks/useMutation.js
@@ -2,30 +2,30 @@ import {API, graphqlOperation} from "aws-amplify";
 
 export const useMutation = ({mutation}) => {
 
-    const CreateMutation = async (input) => {
+  const CreateMutation = async (input) => {
 
-        return await API.graphql(graphqlOperation(mutation, {input: input})).then(({data}) => {
-            return (data && data[Object.keys(data)[0]]);
-        }).catch((err) => {
-            console.log(err);
-        });
-    };
+    return await API.graphql(graphqlOperation(mutation, {input: input})).then(({data}) => {
+      return (data && data[Object.keys(data)[0]]);
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
 
-    const UpdateMutation = async (input) => {
-        return await API.graphql(graphqlOperation(mutation, {input: input})).then(({data}) => {
-            return (data && data[Object.keys(data)[0]]);
-        }).catch((err) => console.log("Error: Mutation " + JSON.stringify(err)));
-    };
+  const UpdateMutation = async (input) => {
+    return await API.graphql(graphqlOperation(mutation, {input: input})).then(({data}) => {
+      return (data && data[Object.keys(data)[0]]);
+    }).catch((err) => console.log("Error: Mutation " + JSON.stringify(err)));
+  };
 
-    const DeleteMutation = async (input) => {
-        return API.graphql(graphqlOperation(mutation, {input: input})).then(({data}) => {
-            return (data && data[Object.keys(data)[0]]);
-        }).catch((err) => console.log("Error: Mutation " + JSON.stringify(err)));
-    };
+  const DeleteMutation = async (input) => {
+    return API.graphql(graphqlOperation(mutation, {input: input})).then(({data}) => {
+      return (data && data[Object.keys(data)[0]]);
+    }).catch((err) => console.log("Error: Mutation " + JSON.stringify(err)));
+  };
 
-    return {
-        editItem: UpdateMutation,
-        deleteItem: DeleteMutation,
-        createItem: CreateMutation,
-    }
+  return {
+    editItem: UpdateMutation,
+    deleteItem: DeleteMutation,
+    createItem: CreateMutation,
+  }
 };
